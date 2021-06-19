1ed2e0878c7d560a93bc116840d8e9bd2b4087c6
Tukaram Bahir <tukaram.bahir@gmail.comdf>
2021-06-19 12:35:56 +0530

    updted db

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2')
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
+  host: 'us-cdbr-east-04.cleardb.com',
+  user: 'ba357698295d31',
   database: 'mystore',
-  password: 'admin123',
+  password: 'c696aff9',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
