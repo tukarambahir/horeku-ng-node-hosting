82405928288cbcfc22cd636c6fb9170693733606
Tukaram Bahir <tukaram.bahir@gmail.comdf>
2021-06-20 09:00:37 +0530

    add

--- server.js
+++ server.js
@@ -41,7 +41,11 @@ app.use('/category', categoryRouter)
 
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static('./dist/medical-site-for-delivery'));
+
+// app.get('/*', (req, res) =>
+//     res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+// );
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
